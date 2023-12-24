30c01f29e8223751eec73177fe9f202e36395353
Pardeep Singh <sainiprince424@gmail.com>
2023-12-24 23:05:13 +0530

    Initial changes
    week-3 03-mongo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://pardeepsingh0920:xgpUyAbIdcCsLALK@cluster0.9f7qem8.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    image: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,40 @@
 const { Router } = require("express");
+const { Admin, Course, User } = require('../db/index');
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+    const body = req.body;
+    const existingAdmin = await Admin.findOne({username: body.username});
+    if(existingAdmin) {
+        res.status(404).send('Username already exists.');
+        return;
+    }
+    Admin.create({
+        username: body.username,
+        password: body.password
+    });
+    res.json({
+        message: 'Admin created successfully'
+    });
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+    const body = req.body;
+    Course.create({
+        title: body.title,
+        description: body.description,
+        price: body.price,
+        image: body.image
+    }).then((createdCourse) => {
+        res.json({ message : 'Course created successfully', courseId: createdCourse.id });
+    });
+    
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+    Course.find({}).then(courses => res.json({courses : courses}));
 });
 
 module.exports = router;
